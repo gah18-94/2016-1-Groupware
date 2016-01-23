91aa62a10176603d33703e46ced23f4626ecfd63
gah18-94 <gah18_94@hotmail.es>
2016-01-23 15:31:12 -0600

    Se corrige el WCF que apuntaba a la interfaz y se crean pruebass

---
+++ Hello.World.Console/Hello.World.WCF.Test/UnitTest1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Hello.World.WCF.Test.MyHelloTest;
+
+namespace Hello.World.WCF.Test
+{
+    [TestClass]
+    public class SayHello
+    {
+        [TestMethod]
+        public void HelloDearUser()
+        {
+            //initialize
+            string username = "Dear User";
+            string expectedResult = "Hello Dear User";
+            string realResult;
+            HelloWorldClient myservice = new HelloWorldClient();
+
+            //test body
+            realResult = myservice.SayHello(username);
+
+            //assert
+            Assert.AreEqual(expectedResult, realResult);
+
+                }
+
+        [TestMethod]
+        public void HelloEmptyUser()
+        {
+            //initialize
+            string username = "";
+            string expectedResult = "Hello ";
+            string realResult;
+            HelloWorldClient myservice = new HelloWorldClient();
+
+            //test body
+            realResult = myservice.SayHello(username);
+
+            //assert
+            Assert.AreEqual(expectedResult, realResult);
+
+        }
+
+        [TestMethod]
+        public void HelloNothingUser()
+        {
+            //initialize
+            string username = null;
+            string expectedResult = "Hello ";
+            string realResult;
+            HelloWorldClient myservice = new HelloWorldClient();
+
+            //test body
+            realResult = myservice.SayHello(username);
+
+            //assert
+            Assert.AreEqual(expectedResult, realResult);
+
+        }
+    }
+}
